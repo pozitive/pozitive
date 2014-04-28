ea000b9b59a316de4d43972959c1c6361344a6a4
Dmitry Sukhikh <china@pozitive.biz>
2014-04-28 14:18:52 +0700

    Fixed spelling mistakes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deploy/apps/poizitive/current"
+root = "/home/deploy/apps/pozitive/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.poizitive.sock"
+listen "/tmp/unicorn.pozitive.sock"
 worker_processes 2
 timeout 30
